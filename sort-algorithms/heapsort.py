0894589f5858f1035b67b63630cee7a690906ab9
Pablo <pablomelo.inf@gmail.com>
2022-10-20 02:13:58 -0300

    [feat] mergeSort, mergeSort spend time O(n(lgn)) to sort array. Add main code to running algorithms

--- sort-algorithms/heapsort.py
+++ sort-algorithms/heapsort.py
@@ -28,15 +28,12 @@ def heapify(arr, sizeHeap, i):
         arr[i], arr[largest] = arr[largest], arr[i]
         heapify(arr, sizeHeap, largest) 
 
-def heapsort(arr):
+def heapSort(arr):
     N = len(arr)
     buildMaxHeap(arr);
 
     for i in range(N-1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i]
         heapify(arr,i, 0)
-
-arr = [7.11,12.97,6.99,5.09,6.96,89.09,98.71,10,12.05,1.3]
-print(arr)
-heapsort(arr)
-print(arr)
+    
+    return arr
